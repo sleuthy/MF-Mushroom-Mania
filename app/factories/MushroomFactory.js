d4af4caf7d5e2ee70e7c698f0e763790287287a5
Kathleen Weisensel <kweisensel@gmail.com>
2017-05-31 15:57:50 -0500

    search filter added

--- app/factories/MushroomFactory.js
+++ app/factories/MushroomFactory.js
@@ -1,21 +1,25 @@
 "use strict";
 
 app.factory("MushroomFactory", function($q, $http){
-
-	let getMushrooms = () => {
-		let items = [];
-		return $q((resolve, reject) => {
-			$http.get('https://mushroom-mania-7edc8.firebaseio.com/.json')
-			.then((itemObject) => {
-				let itemCollection = itemObject.data;
-				console.log("itemCollection", itemCollection);
-				resolve(itemCollection.mushrooms);
-			})
-			.catch((error) => {
-				reject(error);
-			});
-		});
-	};
-
-	return {getMushrooms};
+  let getMushrooms = () => {
+    let items = [];
+    return $q((resolve,reject) => {
+      $http.get('https://mushroom-mania-7edc8.firebaseio.com/.json')
+      .then((itemObject) => {
+        let itemCollection = itemObject.data;
+        let mushroomCollection = itemCollection.mushrooms;
+        console.log("itemCollection", mushroomCollection);
+        for (let item in mushroomCollection) {
+          let currentShroom = mushroomCollection[item];
+          items.push(currentShroom);
+        }
+        console.log("items Array", items);
+        resolve(items);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+  return {getMushrooms};
 });
